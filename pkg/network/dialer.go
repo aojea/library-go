9dde171215631be88b960035964d46badea22455
Antonio Ojea <aojea@redhat.com>
2021-09-24 10:14:04 +0200

    don't use a custom dialer
    Since Kubernetes 1.20, HTTP2 connections run a periodic ping every
    30 seconds to detect stale connections that detect if the connection
    was broken.
    
    This has the advantage over kernel parameters that the connection is
    always alive and you can detect dead peers. Using the Kernel parameters
    is very expensive and agressive, since once the connection is established,
    it stops sending TCP keepalives, and when the TCP_USER_TIMEOUT kicks off for
    not having activity on the connection, it tears down the connection
    ungracefully, without knowing if the peer was really dead, with a high
    risk of closing legit connections.
    
    Golang default parameters for dialer are good, also, overriding the Dial
    function in client-go automatically disables transport caching.
    
    Signed-off-by: Antonio Ojea <aojea@redhat.com>

--- pkg/network/dialer.go
+++ pkg/network/dialer.go
@@ -3,6 +3,7 @@ package network
 import (
 	"context"
 	"net"
+	"time"
 )
 
 type DialContext func(ctx context.Context, network, address string) (net.Conn, error)
@@ -10,4 +11,12 @@ type DialContext func(ctx context.Context, network, address string) (net.Conn, e
 // DefaultDialContext returns a DialContext function from a network dialer with default options sets.
 func DefaultClientDialContext() DialContext {
 	return dialerWithDefaultOptions()
+}
+
+func dialerWithDefaultOptions() DialContext {
+	nd := &net.Dialer{
+		Timeout:   30 * time.Second,
+		KeepAlive: 30 * time.Second,
+	}
+	return nd.DialContext
 }
